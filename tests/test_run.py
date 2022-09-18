68128e4568216ce41eaf6183144bf902cd0b41bd
Phil Hanna <ph1204@gmail.com>
2022-09-17 21:43:24 -0400

    Added test_run() unit test for the -o option

--- tests/test_run.py
+++ tests/test_run.py
@@ -10,8 +10,23 @@ class TestRun(TestCase):
     def setUp(self) -> None:
         testdata = test_data_dir
 
-    def test_run(self):
+    def test_run_l_100(self):
         parms = ["xxd", "-l", "100", "testdata/cut"]
+        cp = subprocess.run(parms, cwd=project_root_dir, stdout=subprocess.PIPE)
+        if cp.returncode != 0:
+            raise RuntimeError(f"Bad return code {cp.returncode} from running {parms[0]}")
+        expected = str(cp.stdout, encoding="utf-8")
+
+        parms[0] = "./pyxxd.py"
+        cp = subprocess.run(parms, cwd=project_root_dir, stdout=subprocess.PIPE)
+        if cp.returncode != 0:
+            raise RuntimeError(f"Bad return code {cp.returncode} from running {parms[0]}")
+        actual = str(cp.stdout, encoding="utf-8")
+
+        self.assertEqual(expected, actual)
+
+    def test_run_o_x20(self):
+        parms = ["xxd", "-l", "100", "-o", "0x20", "testdata/cut"]
         cp = subprocess.run(parms, cwd=project_root_dir, stdout=subprocess.PIPE)
         if cp.returncode != 0:
             raise RuntimeError(f"Bad return code {cp.returncode} from running {parms[0]}")
