25b5caa8a2ee4b9e09699ff771f1069e53535de4
Phil Hanna <ph1204@gmail.com>
2022-09-24 15:11:14 -0400

    Initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pxxd',
+    version='1.0.0',
+    packages=['xxd', 'tests'],
+    url='https://github.com/philhanna/xxd',
+    license='MIT License',
+    author='saspeh',
+    author_email='',
+    description='Python work-alike for xxd'
+)
