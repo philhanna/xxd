5d924a8ef99fd6c43ccd3242bde65ca405ff8098
Phil Hanna <ph1204@gmail.com>
2022-09-28 11:28:58 -0400

    Refactored unit tests
    Refactored unit tests
    
    Refactored unit tests
    
    Refactored unit tests
    
    Refactored unit tests
    
    Refactored unit tests
    
    Refactored unit tests

--- tests/test_reverse.py
+++ tests/test_reverse.py
@@ -4,36 +4,38 @@ import subprocess
 import tempfile
 from unittest import TestCase, skip
 
-from tests import project_root_dir
+from tests import project_root_dir, SaveDirectory
 from xxd import HexDumper
+
+tmp = tempfile.gettempdir()
+
+
+def runxxd(parms) -> subprocess.CompletedProcess:
+    return subprocess.run(parms,
+                          cwd=project_root_dir,
+                          check=True,
+                          text=True,
+                          capture_output=True)
 
 
 class TestReverse(TestCase):
 
     def test_reverse(self):
         # Create the temporary output file for xxd
-        _, infile = tempfile.mkstemp()
+        infile = os.path.join(tmp, "infile")
         indata = "Now is the time for all good men to come to the aid of their party.\n"
         with open(infile, "wt") as fp:
             fp.write(indata)
 
         # Run xxd on the file
-        _, file1 = tempfile.mkstemp()
-        save_cwd = os.getcwd()
-        try:
+        file1 = os.path.join(tmp, "file1")
+        with SaveDirectory():
             os.chdir(project_root_dir)
-            parms = [
-                "xxd",
-                infile,
-                file1
-            ]
-            cp = subprocess.run(parms, stdout=subprocess.PIPE)
-            if cp.returncode != 0:
-                errmsg = f"Bad return code {cp.returncode} from running {parms[0]}"
-                raise RuntimeError(errmsg)
+            parms = ["xxd", infile, file1]
+            runxxd(parms)
 
             # Now run pxxd to reverse the file transformation
-            _, file2 = tempfile.mkstemp()
+            file2 = os.path.join(tmp, "file2")
             args = {
                 "reverse": True,
                 "infile": file1,
@@ -41,8 +43,6 @@ class TestReverse(TestCase):
             }
             app = HexDumper(args)
             app.run()
-        finally:
-            os.chdir(save_cwd)
 
         # Are the files identical?
         self.assertTrue(filecmp.cmp(infile, file2))
@@ -53,28 +53,24 @@ class TestReverse(TestCase):
 
     def test_reverse_with_seek(self):
         # Create the temporary output file for xxd
-        _, infile = tempfile.mkstemp()
+        infile = os.path.join(tmp, "infile")
         indata = "Now is the time for all good men to come to the aid of their party.\n"
         with open(infile, "wt") as fp:
             fp.write(indata)
 
         # Run xxd on the file
-        _, file1 = tempfile.mkstemp()
-        save_cwd = os.getcwd()
-        try:
+        with SaveDirectory():
+            file1 = os.path.join(tmp, "file1")
             os.chdir(project_root_dir)
             parms = [
                 "xxd",
                 infile,
                 file1
             ]
-            cp = subprocess.run(parms, stdout=subprocess.PIPE)
-            if cp.returncode != 0:
-                errmsg = f"Bad return code {cp.returncode} from running {parms[0]}"
-                raise RuntimeError(errmsg)
+            runxxd(parms)
 
             # Now run pxxd to reverse the file transformation
-            _, file2 = tempfile.mkstemp()
+            file2 = os.path.join(tmp, "file2")
             args = {
                 "reverse": True,
                 "seek": 4,
@@ -83,8 +79,6 @@ class TestReverse(TestCase):
             }
             app = HexDumper(args)
             app.run()
-        finally:
-            os.chdir(save_cwd)
 
         # Are the files identical?
         file1bytes = b'\x00\x00\x00\x00' + indata.encode("utf-8")
@@ -98,29 +92,25 @@ class TestReverse(TestCase):
 
     def test_reverse_ps(self):
         # Create the temporary output file for xxd
-        _, infile = tempfile.mkstemp()
+        infile = os.path.join(tmp, "infile")
         indata = "Now is the time for all good men to come to the aid of their party.\n"
         with open(infile, "wt") as fp:
             fp.write(indata)
 
         # Run xxd on the file
-        _, file1 = tempfile.mkstemp()
-        save_cwd = os.getcwd()
-        try:
+        with SaveDirectory():
             os.chdir(project_root_dir)
+            file1 = os.path.join(tmp, "file1")
             parms = [
                 "xxd",
                 "-ps",
                 infile,
                 file1
             ]
-            cp = subprocess.run(parms, stdout=subprocess.PIPE)
-            if cp.returncode != 0:
-                errmsg = f"Bad return code {cp.returncode} from running {parms[0]}"
-                raise RuntimeError(errmsg)
+            runxxd(parms)
 
             # Now run pxxd to reverse the file transformation
-            _, file2 = tempfile.mkstemp()
+            file2 = os.path.join(tmp, "file2")
             args = {
                 "reverse": True,
                 "postscript": True,
@@ -129,8 +119,6 @@ class TestReverse(TestCase):
             }
             app = HexDumper(args)
             app.run()
-        finally:
-            os.chdir(save_cwd)
 
         # Are the files identical?
         self.assertTrue(filecmp.cmp(infile, file2))
@@ -141,29 +129,25 @@ class TestReverse(TestCase):
 
     def test_reverse_ps_with_seek(self):
         # Create the temporary output file for xxd
-        _, infile = tempfile.mkstemp()
+        infile = os.path.join(tmp, "infile")
         indata = "Now is the time for all good men to come to the aid of their party.\n"
         with open(infile, "wt") as fp:
             fp.write(indata)
 
         # Run xxd on the file
-        _, file1 = tempfile.mkstemp()
-        save_cwd = os.getcwd()
-        try:
+        with SaveDirectory():
             os.chdir(project_root_dir)
+            file1 = os.path.join(tmp, "file1")
             parms = [
                 "xxd",
                 "-ps",
                 infile,
                 file1
             ]
-            cp = subprocess.run(parms, stdout=subprocess.PIPE)
-            if cp.returncode != 0:
-                errmsg = f"Bad return code {cp.returncode} from running {parms[0]}"
-                raise RuntimeError(errmsg)
+            runxxd(parms)
 
             # Now run pxxd to reverse the file transformation
-            _, file2 = tempfile.mkstemp()
+            file2 = os.path.join(tmp, "file2")
             args = {
                 "reverse": True,
                 "postscript": True,
@@ -173,8 +157,6 @@ class TestReverse(TestCase):
             }
             app = HexDumper(args)
             app.run()
-        finally:
-            os.chdir(save_cwd)
 
         # Are the files identical?
         file1bytes = b'\x00\x00\x00\x00' + indata.encode("utf-8")
--- tests/test_run.py
+++ tests/test_run.py
@@ -8,23 +8,25 @@ from unittest import TestCase
 from tests import project_root_dir, stdout_redirected, stdin_redirected, SaveDirectory
 from xxd import HexDumper
 
+tmp = tempfile.gettempdir()
+
+
+def runxxd(parms) -> subprocess.CompletedProcess:
+    return subprocess.run(parms,
+                          cwd=project_root_dir,
+                          check=True,
+                          text=True,
+                          capture_output=True)
+
 
 class TestRun(TestCase):
 
-    @staticmethod
-    def runxxd(parms) -> subprocess.CompletedProcess:
-        return subprocess.run(parms,
-                       cwd=project_root_dir,
-                       check=True,
-                       text=True,
-                       stdout=subprocess.PIPE)
-
     def test_default(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "infile": os.path.join(project_root_dir, "testdata/short"),
             "outfile": file2
@@ -38,7 +40,7 @@ class TestRun(TestCase):
 
     def test_l_100(self):
         parms = ["xxd", "-l", "100", "testdata/cut"]
-        cp = self.runxxd(parms)
+        cp = runxxd(parms)
         expected = cp.stdout
 
         args = {
@@ -53,11 +55,11 @@ class TestRun(TestCase):
         self.assertEqual(expected, actual)
 
     def test_o_x20(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-l", "100", "-o", "0x20", "testdata/cut", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "len": 100,
             "offset": "0x20",
@@ -72,11 +74,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_binary(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-b", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "binary": True,
             "infile": os.path.join(project_root_dir, "testdata/short"),
@@ -90,11 +92,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_columns_default(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "infile": os.path.join(project_root_dir, "testdata/short"),
             "outfile": file2
@@ -107,11 +109,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_columns_10(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-u", "-d", "-c", "10", "-o", "0x100", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "cols": 10,
             "decimal": True,
@@ -128,11 +130,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_columns_20(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-c", "20", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "cols": 20,
             "infile": os.path.join(project_root_dir, "testdata/short"),
@@ -156,11 +158,11 @@ class TestRun(TestCase):
         self.assertIn("columns", errmsg)
 
     def test_include(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-i", "-l", "60", "-C", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "include": True,
             "len": 60,
@@ -182,11 +184,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_ebcdic(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-E", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "EBCDIC": True,
             "infile": "testdata/short",
@@ -206,11 +208,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_postscript(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-ps", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "postscript": True,
             "infile": "testdata/short",
@@ -230,11 +232,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_name(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-i", "-n", "3om", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "include": True,
             "name": "3om",
@@ -255,11 +257,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_autoskip_allzero(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-a", "testdata/allzero", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "autoskip": True,
             "infile": "testdata/allzero",
@@ -279,11 +281,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_autoskip_mixed(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-a", "testdata/mixedzero", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "autoskip": True,
             "infile": "testdata/mixedzero",
@@ -300,11 +302,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_seek(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-s", "0x20", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "seek": 0x20,
             "infile": "testdata/short",
@@ -321,11 +323,11 @@ class TestRun(TestCase):
         os.remove(file2)
 
     def test_seek_past_end(self):
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-s", "0x100", "testdata/short", file1]
-        self.runxxd(parms)
+        runxxd(parms)
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "seek": "0x100",
             "infile": "testdata/short",
@@ -342,7 +344,7 @@ class TestRun(TestCase):
 
     def test_seek_with_stdin(self):
         """ Unit test with --seek and stdin"""
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         parms = ["xxd", "-s", "0x01", "-", file1]
         subprocess.run(parms,
                        cwd=project_root_dir,
@@ -351,12 +353,10 @@ class TestRun(TestCase):
                        input="abcdefg",
                        capture_output=True)
 
-        with (
-            StringIO("abcdefg") as fpin,
-            stdin_redirected(fpin),
-            SaveDirectory()
-        ):
-            file2 = os.path.join(tempfile.gettempdir(), "file2")
+        with (StringIO("abcdefg") as fpin,
+              stdin_redirected(fpin),
+              SaveDirectory()):
+            file2 = os.path.join(tmp, "file2")
             args = {
                 "seek": "0x01",
                 "outfile": file2
@@ -371,21 +371,21 @@ class TestRun(TestCase):
 
     def test_dont_show_traceback(self):
         parms = ["./pxxd", "bogus"]
-        cp = self.runxxd(parms)
+        cp = runxxd(parms)
         errmsg = cp.stdout
         self.assertIn("No such file or directory", errmsg)
 
     def test_ps_reverse(self):
         input_string = "4b52414d4552202d2052454d41524b0a"
-        input_pairs = [input_string[i:i+2]
+        input_pairs = [input_string[i:i + 2]
                        for i in range(0, len(input_string), 2)]
         input_chars = [chr(int(x, 16)) for x in input_pairs]
         s = "".join(input_chars)
-        file1 = os.path.join(tempfile.gettempdir(), "file1")
+        file1 = os.path.join(tmp, "file1")
         with open(file1, "wb") as fp:
             fp.write(s.encode("utf-8"))
 
-        file2 = os.path.join(tempfile.gettempdir(), "file2")
+        file2 = os.path.join(tmp, "file2")
         args = {
             "reverse": True,
             "postscript": True,
@@ -403,16 +403,14 @@ class TestRun(TestCase):
 
     def test_ps_reverse_stdout(self):
         input_string = "4b52414d4552202d2052454d41524b"
-        input_pairs = [input_string[i:i+2]
+        input_pairs = [input_string[i:i + 2]
                        for i in range(0, len(input_string), 2)]
         input_chars = [chr(int(x, 16)) for x in input_pairs]
         expected = "".join(input_chars)
-        with (
-            StringIO(input_string) as fpin,
-            stdin_redirected(fpin),
-            StringIO() as fpout,
-            stdout_redirected(fpout)
-        ):
+        with (StringIO(input_string) as fpin,
+              stdin_redirected(fpin),
+              StringIO() as fpout,
+              stdout_redirected(fpout)):
             args = {
                 "reverse": True,
                 "postscript": True,
