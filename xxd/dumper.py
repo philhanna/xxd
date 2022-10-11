afb8f93be5115bdaed73a7d949d6c510fc57cd28
Phil Hanna <ph1204@gmail.com>
2022-10-11 08:35:30 -0400

    Refactored __init__ method

--- xxd/dumper.py
+++ xxd/dumper.py
@@ -1,7 +1,60 @@
-from abc import ABC
+import sys
+from abc import ABC, abstractmethod
 
 
 class Dumper(ABC):
     """Base class for hex dumpers of the three formats"""
     def __init__(self, args: dict):
+        self.args = args
+        self.reverse = None
+        self.outfile = None
+        self.infile = None
+        self.pname: str = sys.argv[0].split("/")[-1]
+        self.fpin = None
+        self.fpout = None
+
+    @abstractmethod
+    def mainline(self):
+        """Runs the dumper"""
         pass
+
+    @abstractmethod
+    def mainline_reverse(self):
+        """Recreates original file from the hex output"""
+        pass
+
+    def run(self):
+        """Calls mainline with specified input and output files"""
+
+        # Check for infile.  If not specified, or if it is "-", use stdin.
+        # Otherwise, try to open the file.
+        # If outfile is specified, open it for writing, otherwise, use stdout.
+
+        self.fpin = None
+        self.fpout = None
+        try:
+            if self.infile is None or self.infile == sys.stdin or self.infile == '-':
+                self.fpin = sys.stdin
+            else:
+                self.fpin = open(self.infile, "rb")
+
+            if self.outfile is None or self.outfile == sys.stdout:
+                self.fpout = sys.stdout
+            else:
+                self.fpout = open(self.outfile, "wb")
+
+            # Run the mainline
+            if self.reverse:
+                self.mainline_reverse()
+            else:
+                self.mainline()
+
+        # Close the files
+        finally:
+            if self.fpin is not None:
+                if self.fpin != sys.stdin:
+                    self.fpin.close()
+            if self.fpout is not None:
+                self.fpout.flush()
+                if self.fpout != sys.stdout:
+                    self.fpout.close()
--- xxd/hex_dumper.py
+++ xxd/hex_dumper.py
@@ -1,5 +1,4 @@
 import os.path
-import pdb
 import re
 import string
 import sys
@@ -277,53 +276,14 @@ class HexDumper(Dumper):
         varname = "".join(valid)
         return varname
 
-    def run(self):
-        """Calls mainline with specified input and output files"""
-
-        # Check for infile.  If not specified, or if it is "-", use stdin.
-        # Otherwise, try to open the file.
-        # If outfile is specified, open it for writing, otherwise, use stdout.
-
-        self.fpin = None
-        self.fpout = None
-        try:
-            if self.infile is None or self.infile == sys.stdin or self.infile == '-':
-                self.fpin = sys.stdin
-            else:
-                self.fpin = open(self.infile, "rb")
-
-            if self.outfile is None or self.outfile == sys.stdout:
-                self.fpout = sys.stdout
-            else:
-                self.fpout = open(self.outfile, "wb")
-
-            # Run the mainline
-            if self.reverse:
-                self.mainline_reverse()
-            else:
-                self.mainline()
-
-        # Close the files
-        finally:
-            if self.fpin is not None:
-                if self.fpin != sys.stdin:
-                    self.fpin.close()
-            if self.fpout is not None:
-                self.fpout.flush()
-                if self.fpout != sys.stdout:
-                    self.fpout.close()
-
     def __init__(self, args: dict = {}):
         """Creates a new XXD object with specified options.
         Note that defaults are implemented here by the dictionary 'get(key, default)' approach.
         Incompatible options raise a ValueError.
         """
 
-        self.args = args
+        super().__init__(args)
 
-        self.pname: str = sys.argv[0].split("/")[-1]
-        self.fpin = None
-        self.fpout = None
         self.autoskip: bool = args.get("autoskip", False)
         self.autoskip_lines = None
         self.autoskip_state = None
@@ -449,7 +409,7 @@ class HexDumper(Dumper):
         self.outfile: str = args.get("outfile", None)
 
     def mainline_reverse(self):
-        """Recreates original file from the hex output"""
+
         if self.seek:
             for i in range(self.seek):
                 self.fpout.write(b'\x00')
