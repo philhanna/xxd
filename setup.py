68195f977f32aee529c925e35f0444944494e170
Phil Hanna <ph1204@gmail.com>
2022-10-11 22:24:03 -0400

    Bumped version number to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pxxd',
-    version='1.0.0',
+    version='1.1.0',
     packages=['xxd', 'tests'],
     url='https://github.com/philhanna/xxd',
     license='MIT License',
