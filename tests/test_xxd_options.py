26efed141a792fd0e16208b75783a8bec4f23c19
Phil Hanna <ph1204@gmail.com>
2022-12-17 21:48:21 -0500

    Parametrized many tests

--- tests/test_xxd_options.py
+++ tests/test_xxd_options.py
@@ -41,116 +41,33 @@ def test_substring_in_errmsg(parms, substring):
     assert substring in errmsg
 
 
-def test_autoskip_true():
-    assert HexDumper({"autoskip": True}).autoskip
+@pytest.mark.parametrize("parms,attrname,expected", [
+    ({"autoskip": True}, "autoskip", True),
+    ({"binary": True}, "cols", 6),
+    ({"decimal": True}, "decimal", True),
+    ({"binary": True}, "octets_per_group", 1),
+    ({"little_endian": True}, "octets_per_group", 4),
+    ({"postscript": True}, "octets_per_group", 2),
+    ({"infile": "/usr/bin/cut"}, "infile", "/usr/bin/cut"),
+    ({"infile": "-"}, "infile", "-"),
+    ({"len": "13"}, "length", 13),
+    ({"len": "0"}, "length", 0),
+    ({"name": "wonderful"}, "name", "wonderful"),
+    ({"offset": 0x100}, "offset", 256),
+    ({"postscript": True}, "postscript", True),
+    ({"reverse": True}, "reverse", True),
+    ({"seek": 0x0100}, "seek", 256),
+    ({"uppercase": True}, "uppercase", True),
+    ({"version": True}, "version", True),
+])
+def test_hexdumper_setters(parms, attrname, expected):
+    xxd = HexDumper(parms)
+    assert getattr(xxd, attrname) == expected
 
 
-def test_cols_binary():
-    xxd = HexDumper({"binary": True})
-    expected = 6
-    actual = xxd.cols
-    assert actual == expected
+def test_postscriptdumper():
+    assert PostscriptDumper({"postscript": True}).cols == 30
 
 
-def test_cols_postscript():
-    xxd = PostscriptDumper({"postscript": True})
-    expected = 30
-    actual = xxd.cols
-    assert actual == expected
-
-
-def test_cols_include():
-    xxd = CDumper({"include": True})
-    expected = 12
-    actual = xxd.cols
-    assert actual == expected
-
-
-def test_decimal():
-    assert HexDumper({"decimal": True}).decimal
-
-
-def test_g_b():
-    xxd = HexDumper({"binary": True})
-    expected = 1
-    actual = xxd.octets_per_group
-    assert actual == expected
-
-
-def test_g_e():
-    xxd = HexDumper({"little_endian": True})
-    expected = 4
-    actual = xxd.octets_per_group
-    assert actual == expected
-
-
-def test_g_ps():
-    xxd = HexDumper({"postscript": True})
-    expected = 2
-    actual = xxd.octets_per_group
-    assert actual == expected
-
-
-def test_infile():
-    xxd = HexDumper({"infile": "/usr/bin/cut"})
-    expected = "/usr/bin/cut"
-    actual = xxd.infile
-    assert actual == expected
-
-
-def test_infile_stdin():
-    xxd = HexDumper({"infile": "-"})
-    expected = "-"
-    actual = xxd.infile
-    assert actual == expected
-
-
-def test_len():
-    xxd = HexDumper({"len": "13"})
-    expected = 13
-    actual = xxd.length
-    assert actual == expected
-
-
-def test_len_zero():
-    xxd = HexDumper({"len": "0"})
-    expected = 0
-    actual = xxd.length
-    assert actual == expected
-
-
-def test_name():
-    xxd = HexDumper({"name": "wonderful"})
-    expected = "wonderful"
-    actual = xxd.name
-    assert actual == expected
-
-
-def test_offset():
-    xxd = HexDumper({"offset": 0x100})
-    expected = 256
-    actual = xxd.offset
-    assert actual == expected
-
-
-def test_postscript():
-    assert HexDumper({"postscript": True}).postscript
-
-
-def test_reverse():
-    assert HexDumper({"reverse": True}).reverse
-
-
-def test_seek():
-    xxd = HexDumper({"seek": 0x0100})
-    expected = 256
-    actual = xxd.seek
-    assert actual == expected
-
-
-def test_uppercase():
-    assert HexDumper({"uppercase": True}).uppercase
-
-
-def test_version():
-    assert HexDumper({"version": True}).version
+def test_cdumper():
+    assert CDumper({"include": True}).cols == 12
