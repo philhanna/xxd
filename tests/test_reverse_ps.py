bfb1a3839c7bc3b2ced56f86c50c5960cd2fe5b8
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:43:12 -0500

    Converted test_reverse_ps to pytest

--- tests/test_reverse_ps.py
+++ tests/test_reverse_ps.py
@@ -1,87 +1,81 @@
 import filecmp
-import os
-from unittest import TestCase
-
+from pathlib import Path
+from os import chdir
 from tests import project_root_dir, SaveDirectory, tmp, runxxd
-from xxd import HexDumper, PostscriptDumper
+from xxd import PostscriptDumper
 
 
-class TestReversePostscript(TestCase):
+def test_reverse_ps(file1, file2):
+    # Create the temporary output file for xxd
+    infile = Path(tmp).joinpath("infile")
+    indata = "Now is the time for all good men to come to the aid of their party.\n"
+    with open(infile, "wt") as fp:
+        fp.write(indata)
 
-    def test_reverse_ps(self):
-        # Create the temporary output file for xxd
-        infile = os.path.join(tmp, "infile")
-        indata = "Now is the time for all good men to come to the aid of their party.\n"
-        with open(infile, "wt") as fp:
-            fp.write(indata)
+    # Run xxd on the file
+    with SaveDirectory():
+        chdir(project_root_dir)
+        parms = [
+            "xxd",
+            "-ps",
+            infile,
+            file1
+        ]
+        runxxd(parms)
 
-        # Run xxd on the file
-        with SaveDirectory():
-            os.chdir(project_root_dir)
-            file1 = os.path.join(tmp, "file1")
-            parms = [
-                "xxd",
-                "-ps",
-                infile,
-                file1
-            ]
-            runxxd(parms)
+        # Now run pxxd to reverse the file transformation
+        args = {
+            "reverse": True,
+            "postscript": True,
+            "infile": file1,
+            "outfile": file2
+        }
+        app = PostscriptDumper(args)
+        app.run()
 
-            # Now run pxxd to reverse the file transformation
-            file2 = os.path.join(tmp, "file2")
-            args = {
-                "reverse": True,
-                "postscript": True,
-                "infile": file1,
-                "outfile": file2
-            }
-            app = PostscriptDumper(args)
-            app.run()
+    # Are the files identical?
+    assert filecmp.cmp(infile, file2)
 
-        # Are the files identical?
-        self.assertTrue(filecmp.cmp(infile, file2))
+    infile.unlink()
+    file1.unlink()
+    file2.unlink()
 
-        os.remove(infile)
-        os.remove(file1)
-        os.remove(file2)
 
-    def test_reverse_ps_with_seek(self):
-        # Create the temporary output file for xxd
-        infile = os.path.join(tmp, "infile")
-        indata = "Now is the time for all good men to come to the aid of their party.\n"
-        with open(infile, "wt") as fp:
-            fp.write(indata)
+def test_reverse_ps_with_seek(file1, file2):
+    # Create the temporary output file for xxd
+    infile = Path(tmp).joinpath("infile")
+    indata = "Now is the time for all good men to come to the aid of their party.\n"
+    with open(infile, "wt") as fp:
+        fp.write(indata)
 
-        # Run xxd on the file
-        with SaveDirectory():
-            os.chdir(project_root_dir)
-            file1 = os.path.join(tmp, "file1")
-            parms = [
-                "xxd",
-                "-ps",
-                infile,
-                file1
-            ]
-            runxxd(parms)
+    # Run xxd on the file
+    with SaveDirectory():
+        chdir(project_root_dir)
+        parms = [
+            "xxd",
+            "-ps",
+            infile,
+            file1
+        ]
+        runxxd(parms)
 
-            # Now run pxxd to reverse the file transformation
-            file2 = os.path.join(tmp, "file2")
-            args = {
-                "reverse": True,
-                "postscript": True,
-                "seek": 4,
-                "infile": file1,
-                "outfile": file2
-            }
-            app = PostscriptDumper(args)
-            app.run()
+        # Now run pxxd to reverse the file transformation
+        args = {
+            "reverse": True,
+            "postscript": True,
+            "seek": 4,
+            "infile": file1,
+            "outfile": file2
+        }
+        app = PostscriptDumper(args)
+        app.run()
 
-        # Are the files identical?
-        file1bytes = b'\x00\x00\x00\x00' + indata.encode("utf-8")
-        with open(file2, "rb") as fp:
-            file2bytes = fp.read()
-        self.assertEqual(file1bytes, file2bytes)
+    # Are the files identical?
+    file1bytes = b'\x00\x00\x00\x00' + indata.encode("utf-8")
+    with open(file2, "rb") as fp:
+        file2bytes = fp.read()
+    assert file1bytes == file2bytes
 
-        os.remove(infile)
-        os.remove(file1)
-        os.remove(file2)
+    infile.unlink()
+    file1.unlink()
+    file2.unlink()
