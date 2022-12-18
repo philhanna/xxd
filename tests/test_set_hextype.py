ef72654f55d13fb4faffc26b43c69da29aece38e
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:52:59 -0500

    Converted test_set_hextype to pytest

--- tests/test_set_hextype.py
+++ tests/test_set_hextype.py
@@ -1,36 +1,13 @@
-from unittest import TestCase
-
+import pytest
 from xxd import HexType, Dumper
 
 
-class TestSetHexType(TestCase):
-
-    def test_binary(self):
-        args = {"binary": True}
-        expected = HexType.HEX_BITS
-        actual = Dumper.set_hextype(args)
-        self.assertEqual(expected, actual)
-
-    def test_little_endian(self):
-        args = {"little_endian": True}
-        expected = HexType.HEX_LITTLEENDIAN
-        actual = Dumper.set_hextype(args)
-        self.assertEqual(expected, actual)
-
-    def test_c_include(self):
-        args = {"include": True}
-        expected = HexType.HEX_CINCLUDE
-        actual = Dumper.set_hextype(args)
-        self.assertEqual(expected, actual)
-
-    def test_postscript(self):
-        args = {"postscript": True}
-        expected = HexType.HEX_POSTSCRIPT
-        actual = Dumper.set_hextype(args)
-        self.assertEqual(expected, actual)
-
-    def test_normal(self):
-        args = {}
-        expected = HexType.HEX_NORMAL
-        actual = Dumper.set_hextype(args)
-        self.assertEqual(expected, actual)
+@pytest.mark.parametrize("parms, expected", [
+    ({}, HexType.HEX_NORMAL),
+    ({"binary": True}, HexType.HEX_BITS),
+    ({"little_endian": True}, HexType.HEX_LITTLEENDIAN),
+    ({"include": True}, HexType.HEX_CINCLUDE),
+    ({"postscript": True}, HexType.HEX_POSTSCRIPT),
+])
+def test_setting(parms, expected):
+    assert Dumper.set_hextype(parms) == expected
