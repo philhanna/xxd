9f5fda56d81db23d616e9223e960f8036debddaf
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:34:45 -0500

    Converted test_hex_type to pytest

--- tests/test_hex_type.py
+++ tests/test_hex_type.py
@@ -1,10 +1,5 @@
-from unittest import TestCase
-
 from xxd import HexType
 
 
-class TestHexType(TestCase):
-    def test_for_circular_import(self):
-        expected = 3
-        actual = HexType.HEX_BITS.value
-        self.assertEqual(expected, actual)
+def test_for_circular_import():
+    assert HexType.HEX_BITS.value == 3
