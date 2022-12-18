b99f4daaa69047a1093b7a4ab195d33084bbc46b
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:33:28 -0500

    Converted test_dumper_run_ps to pytest

--- tests/test_dumper_run_ps.py
+++ tests/test_dumper_run_ps.py
@@ -1,8 +1,7 @@
 import filecmp
-import os
-import subprocess
 from io import StringIO
-from unittest import TestCase
+from os import chdir
+from pathlib import Path
 
 from tests import project_root_dir, stdout_redirected, stdin_redirected, SaveDirectory, tmp, runxxd, testdata
 from xxd import PostscriptDumper
@@ -11,82 +10,79 @@ CPGM = "xxd"
 PPGM = "./pxxd"
 
 
-class TestDumperRunPostscript(TestCase):
+def test_postscript(file1, file2):
+    parms = [CPGM, "-ps", "testdata/short", file1]
+    runxxd(parms)
 
-    def test_postscript(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-ps", "testdata/short", file1]
-        runxxd(parms)
+    parms = [PPGM, "-ps", "testdata/short", file2]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-ps", "testdata/short", file2]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
 
-    def test5(self):
-        """Test 5: Print 120 bytes as continuous hexdump with 20 octets per line"""
-        infile = os.path.join(testdata, "xxd.1")
-        with StringIO() as fp, stdout_redirected(fp):
-            args = {
-                "postscript": True,
-                "len": 120,
-                "cols": 20,
-                "infile": infile
-            }
-            app = PostscriptDumper(args)
-            app.run()
-            actual = fp.getvalue()
+def test5():
+    """Test 5: Print 120 bytes as continuous hexdump with 20 octets per line"""
+    infile = Path(testdata).joinpath("xxd.1")
+    with StringIO() as fp, stdout_redirected(fp):
+        args = {
+            "postscript": True,
+            "len": 120,
+            "cols": 20,
+            "infile": infile
+        }
+        app = PostscriptDumper(args)
+        app.run()
+        actual = fp.getvalue()
 
-        filename = os.path.join(testdata, "man_copy.ps.expected")
-        with open(filename, "rt") as fp:
-            expected = fp.read()
+    filename = Path(testdata).joinpath("man_copy.ps.expected")
+    with open(filename, "rt") as fp:
+        expected = fp.read()
 
-        self.assertEqual(expected, actual)
+    assert actual == expected
 
-    def test_ps_reverse(self):
-        input_string = "4b52414d4552202d2052454d41524b0a"
-        input_pairs = [input_string[i:i + 2]
-                       for i in range(0, len(input_string), 2)]
-        input_chars = [chr(int(x, 16)) for x in input_pairs]
-        s = "".join(input_chars)
-        file1 = os.path.join(tmp, "file1")
-        with open(file1, "wb") as fp:
-            fp.write(s.encode("utf-8"))
 
-        file2 = os.path.join(tmp, "file2")
+def test_ps_reverse(file1, file2):
+    input_string = "4b52414d4552202d2052454d41524b0a"
+    input_pairs = [input_string[i:i + 2]
+                   for i in range(0, len(input_string), 2)]
+    input_chars = [chr(int(x, 16)) for x in input_pairs]
+    s = "".join(input_chars)
+    with open(file1, "wb") as fp:
+        fp.write(s.encode("utf-8"))
+
+    args = {
+        "reverse": True,
+        "postscript": True,
+        "infile": "testdata/shortline",
+        "outfile": file2,
+    }
+    with SaveDirectory():
+        chdir(project_root_dir)
+        app = PostscriptDumper(args)
+        app.run()
+
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
+
+
+def test_ps_reverse_stdout():
+    input_string = "4b52414d4552202d2052454d41524b"
+    input_pairs = [input_string[i:i + 2]
+                   for i in range(0, len(input_string), 2)]
+    input_chars = [chr(int(x, 16)) for x in input_pairs]
+    expected = "".join(input_chars)
+    with (StringIO(input_string) as fpin,
+          stdin_redirected(fpin),
+          StringIO() as fpout,
+          stdout_redirected(fpout)):
         args = {
             "reverse": True,
             "postscript": True,
-            "infile": "testdata/shortline",
-            "outfile": file2,
         }
-        with SaveDirectory():
-            os.chdir(project_root_dir)
-            app = PostscriptDumper(args)
-            app.run()
-
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
-
-    def test_ps_reverse_stdout(self):
-        input_string = "4b52414d4552202d2052454d41524b"
-        input_pairs = [input_string[i:i + 2]
-                       for i in range(0, len(input_string), 2)]
-        input_chars = [chr(int(x, 16)) for x in input_pairs]
-        expected = "".join(input_chars)
-        with (StringIO(input_string) as fpin,
-              stdin_redirected(fpin),
-              StringIO() as fpout,
-              stdout_redirected(fpout)):
-            args = {
-                "reverse": True,
-                "postscript": True,
-            }
-            app = PostscriptDumper(args)
-            app.run()
-            actual = fpout.getvalue()
-        self.assertEqual(expected, actual)
+        app = PostscriptDumper(args)
+        app.run()
+        actual = fpout.getvalue()
+    assert actual == expected
