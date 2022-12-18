3ff0a44968ffae24b4279d6df2f1d1e5cb5f0a8d
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:47:36 -0500

    Converted test_set_binary to pytest

--- tests/test_set_binary.py
+++ tests/test_set_binary.py
@@ -1,31 +1,22 @@
-from unittest import TestCase
-
+import pytest
 from xxd import Dumper
 
 
-class TestSetBinary(TestCase):
+def test_bogus():
+    args = {"binary": "bogus"}
+    with pytest.raises(ValueError) as ve:
+        Dumper.set_binary(args)
+    errmsg = str(ve.value)
+    assert "is not True or False" in errmsg
 
-    def test_bogus(self):
-        args = {"binary": "bogus"}
-        with self.assertRaises(ValueError) as ve:
-            Dumper.set_binary(args)
-        errmsg = str(ve.exception)
-        self.assertIn("is not True or False", errmsg)
 
-    def test_default(self):
-        args = {}
-        expected = False
-        actual = Dumper.set_binary(args)
-        self.assertEqual(expected, actual)
+def test_default():
+    assert not Dumper.set_binary({})
 
-    def test_false(self):
-        args = {"binary": False}
-        expected = False
-        actual = Dumper.set_binary(args)
-        self.assertEqual(expected, actual)
 
-    def test_true(self):
-        args = {"binary": True}
-        expected = True
-        actual = Dumper.set_binary(args)
-        self.assertEqual(expected, actual)
+def test_false():
+    assert not Dumper.set_binary({"binary": False})
+
+
+def test_true():
+    assert Dumper.set_binary({"binary": True})
