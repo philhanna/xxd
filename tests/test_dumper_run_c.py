7c6c3ce4f56306b7ec6ce8027bcbc7ca3276cf20
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:19:40 -0500

    Converted test_dumper_run_c to pytest

--- tests/test_dumper_run_c.py
+++ tests/test_dumper_run_c.py
@@ -1,7 +1,6 @@
 import filecmp
-import os
 from io import StringIO
-from unittest import TestCase
+from pathlib import Path
 
 from tests import tmp, runxxd, SaveDirectory, project_root_dir, stdout_redirected
 from xxd import CDumper
@@ -10,82 +9,80 @@ CPGM = "xxd"
 PPGM = "./pxxd"
 
 
-class TestDumperRunC(TestCase):
+def test_include(file1, file2):
+    parms = [CPGM, "-i", "-l", "60", "-C", "testdata/short", file1]
+    runxxd(parms)
 
-    def test_include(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-i", "-l", "60", "-C", "testdata/short", file1]
-        runxxd(parms)
+    parms = [PPGM, "-i", "-l", "60", "-C", "testdata/short", file2]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-i", "-l", "60", "-C", "testdata/short", file2]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
 
-    def test_varname(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-i", "-n", "3om", "testdata/short", file1]
-        runxxd(parms)
+def test_varname(file1, file2):
+    parms = [CPGM, "-i", "-n", "3om", "testdata/short", file1]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-i", "-n", "3om", "testdata/short", file2]
-        runxxd(parms)
+    parms = [PPGM, "-i", "-n", "3om", "testdata/short", file2]
+    runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-    def test_vim_7(self):
-        """Test 7: Print C include"""
-        indata = "TESTabcd09\n"
-        file1 = os.path.join(tmp, "XXDFile")
-        with open(file1, "wt") as fp:
-            fp.write(indata)
 
-        with StringIO() as out, stdout_redirected(out):
-            args = {
-                "include": True,
-                "infile": file1,
-                "name": "XXDFile",
-            }
-            app = CDumper(args)
-            app.run()
-            actual = out.getvalue()
+def test_vim_7():
+    """Test 7: Print C include"""
+    indata = "TESTabcd09\n"
+    file1 = Path(tmp).joinpath("XXDFile")
+    with open(file1, "wt") as fp:
+        fp.write(indata)
 
-        expected = """\
+    with StringIO() as out, stdout_redirected(out):
+        args = {
+            "include": True,
+            "infile": file1,
+            "name": "XXDFile",
+        }
+        app = CDumper(args)
+        app.run()
+        actual = out.getvalue()
+
+    expected = """\
 unsigned char XXDFile[] = {
   0x54, 0x45, 0x53, 0x54, 0x61, 0x62, 0x63, 0x64, 0x30, 0x39, 0x0a
 };
 unsigned int XXDFile_len = 11;
 """
-        self.assertEqual(expected, actual)
-        os.remove(file1)
+    assert actual == expected
+    file1.unlink()
 
-    def test_vim_8(self):
-        """Test 8: Print C include capitalized"""
-        indata = "TESTabcd09\n"
-        file1 = os.path.join(tmp, "XXDFile")
-        with open(file1, "wt") as fp:
-            fp.write(indata)
 
-        with StringIO() as out, stdout_redirected(out):
-            args = {
-                "include": True,
-                "infile": file1,
-                "capitalize": True,
-                "name": "XXDFile",
-            }
-            app = CDumper(args)
-            app.run()
-            actual = out.getvalue()
+def test_vim_8():
+    """Test 8: Print C include capitalized"""
+    indata = "TESTabcd09\n"
+    file1 = Path(tmp).joinpath("XXDFile")
+    with open(file1, "wt") as fp:
+        fp.write(indata)
 
-        expected = """\
+    with StringIO() as out, stdout_redirected(out):
+        args = {
+            "include": True,
+            "infile": file1,
+            "capitalize": True,
+            "name": "XXDFile",
+        }
+        app = CDumper(args)
+        app.run()
+        actual = out.getvalue()
+
+    expected = """\
 unsigned char XXDFILE[] = {
   0x54, 0x45, 0x53, 0x54, 0x61, 0x62, 0x63, 0x64, 0x30, 0x39, 0x0a
 };
 unsigned int XXDFILE_LEN = 11;
 """
-        self.assertEqual(expected, actual)
+    assert actual == expected
+    file1.unlink()
