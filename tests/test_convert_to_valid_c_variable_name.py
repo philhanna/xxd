e58dc4a3d0c717e6a85afd9ad540d5a608753e40
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:09:14 -0500

    Converted test_convert_to_valid_c_variable_name to pytest

--- tests/test_convert_to_valid_c_variable_name.py
+++ tests/test_convert_to_valid_c_variable_name.py
@@ -1,36 +1,13 @@
-from unittest import TestCase
+import pytest
+from xxd import CDumper
 
-from xxd import HexDumper, CDumper
 
-
-class TestConvertToValidCVariableName(TestCase):
-
-    def test_0(self):
-        name = "foo"
-        expected = "foo"
-        actual = CDumper.convert_to_valid_c_variable_name(name)
-        self.assertEqual(expected, actual)
-
-    def test_1(self):
-        bad_name = "3"
-        expected = "__3"
-        actual = CDumper.convert_to_valid_c_variable_name(bad_name)
-        self.assertEqual(expected, actual)
-
-    def test_2(self):
-        bad_name = "testdata/short"
-        expected = "testdata_short"
-        actual = CDumper.convert_to_valid_c_variable_name(bad_name)
-        self.assertEqual(expected, actual)
-
-    def test_3(self):
-        bad_name = "3testdata/short"
-        expected = "__3testdata_short"
-        actual = CDumper.convert_to_valid_c_variable_name(bad_name)
-        self.assertEqual(expected, actual)
-
-    def test_4(self):
-        bad_name = " "
-        expected = "_"
-        actual = CDumper.convert_to_valid_c_variable_name(bad_name)
-        self.assertEqual(expected, actual)
+@pytest.mark.parametrize("name,expected", [
+    ("foo", "foo"),
+    ("3", "__3"),
+    ("testdata/short", "testdata_short"),
+    ("3testdata/short", "__3testdata_short"),
+    (" ", "_"),
+])
+def test_conversion(name, expected):
+    assert CDumper.convert_to_valid_c_variable_name(name) == expected
