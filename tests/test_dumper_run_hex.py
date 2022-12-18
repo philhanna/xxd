e6c38719112853d5c9f3db5fdbfe8de1eca0442f
Phil Hanna <ph1204@gmail.com>
2022-12-17 22:29:17 -0500

    Converted test_dumper_run_hex to pytest

--- tests/test_dumper_run_hex.py
+++ tests/test_dumper_run_hex.py
@@ -1,8 +1,8 @@
 import filecmp
-import os
 import subprocess
-from io import StringIO
-from unittest import TestCase
+from pathlib import Path
+
+import pytest
 
 from tests import project_root_dir, stdout_redirected, stdin_redirected, SaveDirectory, tmp, runxxd
 from xxd import HexDumper
@@ -11,198 +11,186 @@ CPGM = "xxd"
 PPGM = "./pxxd"
 
 
-class TestDumperRunHex(TestCase):
+def test_default(file1, file2):
+    parms = [CPGM, "testdata/short", file1]
+    runxxd(parms)
 
-    def test_default(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "testdata/short", file1]
-        runxxd(parms)
+    parms = [PPGM, "testdata/short", file2]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "testdata/short", file2]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
 
-    def test_l_100(self):
-        parms = [CPGM, "-l", "100", "testdata/cut"]
-        cp = runxxd(parms)
-        expected = cp.stdout
+def test_l_100():
+    parms = [CPGM, "-l", "100", "testdata/cut"]
+    cp = runxxd(parms)
+    expected = cp.stdout
 
-        parms = [PPGM, "-l", "100", "testdata/cut"]
-        cp = runxxd(parms)
-        actual = cp.stdout
+    parms = [PPGM, "-l", "100", "testdata/cut"]
+    cp = runxxd(parms)
+    actual = cp.stdout
 
-        self.assertEqual(expected, actual)
+    assert actual == expected
 
-    def test_o_x20(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-l", "100", "-o", "0x20", "testdata/cut", file1]
-        runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-l", "100", "-o", "0x20", "testdata/cut", file2]
-        runxxd(parms)
+def test_o_x20(file1, file2):
+    parms = [CPGM, "-l", "100", "-o", "0x20", "testdata/cut", file1]
+    runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+    parms = [PPGM, "-l", "100", "-o", "0x20", "testdata/cut", file2]
+    runxxd(parms)
 
-    def test_binary(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-b", "testdata/short", file1]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-b", "testdata/short", file2]
-        runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+def test_binary(file1, file2):
+    parms = [CPGM, "-b", "testdata/short", file1]
+    runxxd(parms)
 
-    def test_columns_default(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "testdata/short", file1]
-        runxxd(parms)
+    parms = [PPGM, "-b", "testdata/short", file2]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "testdata/short", file2]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
 
-    def test_columns_10(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-u", "-d", "-c", "10", "-o", "0x100", "testdata/short", file1]
-        runxxd(parms)
+def test_columns_default(file1, file2):
+    parms = [CPGM, "testdata/short", file1]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [CPGM, "-u", "-d", "-c", "10", "-o", "0x100", "testdata/short", file2]
-        runxxd(parms)
+    parms = [PPGM, "testdata/short", file2]
+    runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-    def test_columns_20(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-c", "20", "testdata/short", file1]
-        runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-c", "20", "testdata/short", file2]
-        runxxd(parms)
+def test_columns_10(file1, file2):
+    parms = [CPGM, "-u", "-d", "-c", "10", "-o", "0x100", "testdata/short", file1]
+    runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+    parms = [CPGM, "-u", "-d", "-c", "10", "-o", "0x100", "testdata/short", file2]
+    runxxd(parms)
 
-    def test_columns_too_great(self):
-        args = {
-            "cols": 2000,
-            "infile": os.path.join(project_root_dir, "testdata/short"),
-        }
-        with self.assertRaises(ValueError) as x:
-            HexDumper(args)
-        errmsg = str(x.exception)
-        self.assertIn("columns", errmsg)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-    def test_ebcdic(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-E", "testdata/short", file1]
-        runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-E", "testdata/short", file2]
-        runxxd(parms)
+def test_columns_20(file1, file2):
+    parms = [CPGM, "-c", "20", "testdata/short", file1]
+    runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+    parms = [PPGM, "-c", "20", "testdata/short", file2]
+    runxxd(parms)
 
-    def test_autoskip_allzero(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-a", "testdata/allzero", file1]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-a", "testdata/allzero", file2]
-        runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+def test_columns_too_great():
+    args = {
+        "cols": 2000,
+        "infile": Path(project_root_dir).joinpath("testdata/short"),
+    }
+    with pytest.raises(ValueError) as x:
+        HexDumper(args)
+    errmsg = str(x.value)
+    assert "columns" in errmsg
 
-    def test_autoskip_mixed(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-a", "testdata/mixedzero", file1]
-        runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-a", "testdata/mixedzero", file2]
-        runxxd(parms)
+def test_ebcdic(file1, file2):
+    parms = [CPGM, "-E", "testdata/short", file1]
+    runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+    parms = [PPGM, "-E", "testdata/short", file2]
+    runxxd(parms)
 
-    def test_seek(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-s", "0x20", "testdata/short", file1]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-s", "0x20", "testdata/short", file2]
-        runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+def test_autoskip_allzero(file1, file2):
+    parms = [CPGM, "-a", "testdata/allzero", file1]
+    runxxd(parms)
 
-    def test_seek_past_end(self):
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-s", "0x100", "testdata/short", file1]
-        runxxd(parms)
+    parms = [PPGM, "-a", "testdata/allzero", file2]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-s", "0x100", "testdata/short", file2]
-        runxxd(parms)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
 
-    def test_seek_with_stdin(self):
-        """ Unit test with --seek and stdin"""
-        file1 = os.path.join(tmp, "file1")
-        parms = [CPGM, "-s", "0x01", "-", file1]
-        subprocess.run(parms,
-                       cwd=project_root_dir,
-                       check=True,
-                       text=True,
-                       input="abcdefg",
-                       capture_output=True)
+def test_autoskip_mixed(file1, file2):
+    parms = [CPGM, "-a", "testdata/mixedzero", file1]
+    runxxd(parms)
 
-        file2 = os.path.join(tmp, "file2")
-        parms = [PPGM, "-s", "0x01", "-", file2]
-        subprocess.run(parms,
-                       cwd=project_root_dir,
-                       check=True,
-                       text=True,
-                       input="abcdefg",
-                       capture_output=True)
+    parms = [PPGM, "-a", "testdata/mixedzero", file2]
+    runxxd(parms)
 
-        self.assertTrue(filecmp.cmp(file1, file2))
-        os.remove(file1)
-        os.remove(file2)
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
 
-    def test_dont_show_traceback(self):
-        parms = [PPGM, "bogus"]
-        cp = runxxd(parms)
-        errmsg = cp.stdout
-        self.assertIn("No such file or directory", errmsg)
+
+def test_seek(file1, file2):
+    parms = [CPGM, "-s", "0x20", "testdata/short", file1]
+    runxxd(parms)
+
+    parms = [PPGM, "-s", "0x20", "testdata/short", file2]
+    runxxd(parms)
+
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
+
+
+def test_seek_past_end(file1, file2):
+    parms = [CPGM, "-s", "0x100", "testdata/short", file1]
+    runxxd(parms)
+
+    parms = [PPGM, "-s", "0x100", "testdata/short", file2]
+    runxxd(parms)
+
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
+
+
+def test_seek_with_stdin(file1, file2):
+    """ Unit test with --seek and stdin"""
+    parms = [CPGM, "-s", "0x01", "-", file1]
+    subprocess.run(parms,
+                   cwd=project_root_dir,
+                   check=True,
+                   text=True,
+                   input="abcdefg",
+                   capture_output=True)
+
+    parms = [PPGM, "-s", "0x01", "-", file2]
+    subprocess.run(parms,
+                   cwd=project_root_dir,
+                   check=True,
+                   text=True,
+                   input="abcdefg",
+                   capture_output=True)
+
+    assert filecmp.cmp(file1, file2)
+    file1.unlink()
+    file2.unlink()
+
+
+def test_dont_show_traceback():
+    parms = [PPGM, "bogus"]
+    cp = runxxd(parms)
+    errmsg = cp.stdout
+    assert "No such file or directory" in errmsg
